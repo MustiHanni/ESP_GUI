c76079558bdacc745d8a6ee3a84e82555fc41092
mustapha aref <mustapha.aref@rwth-aachen.de>
2017-10-15 15:01:21 +0200

    Add widgets

--- ESP_GUI/Form1.Designer.cs
+++ ESP_GUI/Form1.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SendButton = new System.Windows.Forms.Button();
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.InputNrUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.InputNrUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(426, 236);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(100, 41);
+            this.SendButton.TabIndex = 0;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputLabel.Location = new System.Drawing.Point(12, 36);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(46, 21);
+            this.InputLabel.TabIndex = 1;
+            this.InputLabel.Text = "Input";
+            this.InputLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // InputNrUpDown
+            // 
+            this.InputNrUpDown.Location = new System.Drawing.Point(119, 38);
+            this.InputNrUpDown.Name = "InputNrUpDown";
+            this.InputNrUpDown.Size = new System.Drawing.Size(120, 20);
+            this.InputNrUpDown.TabIndex = 2;
+            this.InputNrUpDown.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(538, 289);
+            this.Controls.Add(this.InputNrUpDown);
+            this.Controls.Add(this.InputLabel);
+            this.Controls.Add(this.SendButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Connect_ESP";
+            ((System.ComponentModel.ISupportInitialize)(this.InputNrUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.NumericUpDown InputNrUpDown;
     }
 }
 
--- ESP_GUI/Form1.cs
+++ ESP_GUI/Form1.cs
@@ -16,5 +16,20 @@ namespace ESP_GUI
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
